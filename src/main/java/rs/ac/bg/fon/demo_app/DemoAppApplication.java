10a4ee2c04ef7fdd7c1adcc903c0519772d77f7f
Uros Vesic <uros.vesic@fioneer.com>
2025-06-20 22:56:55 +0200

    first commit

---
+++ src/main/java/rs/ac/bg/fon/demo_app/DemoAppApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+
+}
